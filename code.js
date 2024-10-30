84118ab0ce094dd0f8971000c85a15f80f820319
razzaman <razzaman9834@gmail.com>
2024-10-30 08:01:13 +0530

    testtt

---
+++ code.js
@@ -0,0 +1,12 @@
+function generateRandomString(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
+// Usage example:
+const randomString = generateRandomString(10);
+console.log("Random string:", randomString);
